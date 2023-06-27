b5fa43b317444fb1ed21d77d3b7cdcdd7b1bd85e
Wiyam <john.alonalon12@gmail.com>
2023-06-27 09:30:54 +0800

    second commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,265 @@
+import 'package:customer/components/navbar.dart';
+import 'package:customer/pages/addorder.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool first = true;
+  String username = 'Username';
+  List<String> buttonLabels = [
+    'Tapsi',
+    'Beverages',
+  ];
+  List<List<dynamic>> menu = [
+    ['Product 1', 'Price 1', '15-20 minutes', AssetImage('images/ambot.png')],
+    ['Product 2', 'Price 2', '10-15 minutes', AssetImage('images/ambot.png')],
+    ['Product 1', 'Price 1', '15-20 minutes', AssetImage('images/ambot.png')],
+    ['Product 2', 'Price 2', '10-15 minutes', AssetImage('images/ambot.png')],
+    ['Product 1', 'Price 1', '15-20 minutes', AssetImage('images/ambot.png')],
+    ['Product 2', 'Price 2', '10-15 minutes', AssetImage('images/ambot.png')],
+    ['Product 1', 'Price 1', '15-20 minutes', AssetImage('images/ambot.png')],
+    ['Product 2', 'Price 2', '10-15 minutes', AssetImage('images/ambot.png')],
+    ['Product 1', 'Price 1', '15-20 minutes', AssetImage('images/ambot.png')],
+    ['Product 2', 'Price 2', '10-15 minutes', AssetImage('images/ambot.png')],
+    ['Product 1', 'Price 1', '15-20 minutes', AssetImage('images/ambot.png')],
+    ['Product 2', 'Price 2', '10-15 minutes', AssetImage('images/ambot.png')],
+
+    // ['Product 3', 'Price 3', 'Details 3', AssetImage('images/ambot.png')],
+  ];
+  // Labels for the buttons
+  int selectedIndex = 0; // Track the index of the selected button
+  @override
+  Widget build(BuildContext context) {
+    if (first) {
+      first = false;
+      buttonLabels.insert(0, 'All');
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color(0xFFa02e49),
+        actions: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Hello $username,',
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Color.fromARGB(255, 207, 207, 207)),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      drawer: SideNavBar(),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(0xFFa02e49),
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16.0),
+                      child: Text(
+                        'Welcome!',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search, color: Color(0xFFb8a3a8)),
+                    hintText: 'Search',
+                    filled: true,
+                    fillColor: Color(0xFFbc8390),
+                    hintStyle: TextStyle(color: Color(0xFFb8a3a8)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                SizedBox(
+                  height: 40.0,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: buttonLabels.length,
+                    itemBuilder: (context, index) {
+                      bool isSelected = selectedIndex == index;
+                      Color backgroundColor =
+                          isSelected ? Colors.black : Colors.white;
+                      Color textColor =
+                          isSelected ? Colors.white : Colors.black;
+
+                      return Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                backgroundColor),
+                          ),
+                          child: Text(
+                            buttonLabels[index],
+                            style: TextStyle(color: textColor),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.65,
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: menu.length,
+                    itemBuilder: (context, index) {
+                      String productName = menu[index][0];
+                      String price = menu[index][1];
+                      String details = menu[index][2];
+                      AssetImage image = menu[index][3];
+
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => AddOrder(),
+                            ),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Container(
+                            height: 120,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20)),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Container(
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(20),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.5),
+                                          spreadRadius: 2,
+                                          blurRadius: 5,
+                                          offset: Offset(0, 3),
+                                        ),
+                                      ],
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Image(
+                                        image: image,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 8),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '$productName',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.timer,
+                                          color: Colors.grey[300],
+                                        ),
+                                        Text('$details',
+                                            style: TextStyle(
+                                                color: Colors.grey[400])),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.5,
+                                      child: Container(
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              '₱ $price',
+                                              style: TextStyle(
+                                                  color: Colors.pinkAccent,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 20),
+                                            ),
+                                            Container(
+                                                decoration: BoxDecoration(
+                                                  color: Colors.white,
+                                                  boxShadow: [
+                                                    BoxShadow(
+                                                      color: Colors.grey
+                                                          .withOpacity(0.5),
+                                                      spreadRadius: 2,
+                                                      blurRadius: 5,
+                                                      offset: Offset(0, 3),
+                                                    ),
+                                                  ],
+                                                ),
+                                                child: Icon(Icons.add))
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
